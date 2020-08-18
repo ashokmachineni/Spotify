6e0c1666d693484fea89fd27318926de897c6d5d
ashokmachineni <ashok5679m@gmail.com>
2020-08-18 10:48:07 +0100

    finished update username

--- src/components/Settings/UserName.js
+++ src/components/Settings/UserName.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Button, Input } from "semantic-ui-react";
 import { toast } from "react-toastify";
 import firebase from "../../utils/Firebase";
 import "firebase/auth";
 export default function UserName(props) {
-  const { user } = props;
+  const {
+    user,
+    setShowModel,
+    setTitleModel,
+    setContentModel,
+    setReloadApp
+  } = props;
   const onEdit = () => {
-    console.log("its time to edit");
+    setTitleModel("Update Name");
+    setContentModel(
+      <ChangeDisplayNameForm
+        displayName={user.displayName}
+        setShowModel={setShowModel}
+        setReloadApp={setReloadApp}
+      />
+    );
+    setShowModel(true);
   };
   return (
     <div className="user-name">
@@ -15,5 +29,42 @@ export default function UserName(props) {
         Update
       </Button>
     </div>
+  );
+}
+function ChangeDisplayNameForm(props) {
+  const { displayName, setShowModel, setReloadApp } = props;
+  const [formData, setFormData] = useState({ displayName: displayName });
+  const [isLoading, setIsLoading] = useState(false);
+  const onSubmit = () => {
+    if (!formData.displayName || formData.displayName === displayName) {
+      setShowModel(false);
+    } else {
+      setIsLoading(true);
+      firebase
+        .auth()
+        .currentUser.updateProfile({ displayName: formData.displayName })
+        .then(() => {
+          setReloadApp(prevState => !prevState);
+          toast.success("username updated successfully");
+          setIsLoading(false);
+          setShowModel(false);
+        })
+        .catch(() => {
+          toast.error("update name is failed");
+        });
+    }
+  };
+  return (
+    <Form onSubmit={onSubmit}>
+      <Form.Field>
+        <Input
+          defaultValue={displayName}
+          onChange={e => setFormData({ displayName: e.target.value })}
+        />
+      </Form.Field>
+      <Button type="submit" loading={isLoading}>
+        Update
+      </Button>
+    </Form>
   );
 }
--- src/pages/Settings/Settings.js
+++ src/pages/Settings/Settings.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import UploadAvatar from "../../components/Settings/UploadAvatar";
 import UserName from "../../components/Settings/UserName";
 import BasicModel from "../../components/Model/BasicModel";
@@ -6,16 +6,25 @@ import "./Settings.scss";
 
 export default function Settings(props) {
   const { user, setReloadApp } = props;
+  const [showModel, setShowModel] = useState(false);
+  const [titleModel, setTitleModel] = useState("");
+  const [contentModel, setContentModel] = useState(null);
 
   return (
     <div className="settings">
       <h1>Configuration</h1>
       <div className="avatar-name">
         <UploadAvatar user={user} setReloadApp={setReloadApp} />
-        <UserName user={user} />
+        <UserName
+          user={user}
+          setShowModel={setShowModel}
+          setTitleModel={setTitleModel}
+          setContentModel={setContentModel}
+          setReloadApp={setReloadApp}
+        />
       </div>
-      <BasicModel show={true} setShow={false} title={"HOLA"}>
-        <h3>FORM</h3>
+      <BasicModel show={showModel} setShow={setShowModel} title={titleModel}>
+        {contentModel}
       </BasicModel>
     </div>
   );
