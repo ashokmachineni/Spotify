6c3aacaa78bfe55824f9fe7e4a1bc2ea7fb55722
ashokmachineni <ashok5679m@gmail.com>
2020-08-29 00:28:44 +0100

    getting all albums of each artist

--- src/pages/Album/Album.js
+++ src/pages/Album/Album.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { withRouter } from "react-router-dom";
+import { Loader } from "semantic-ui-react";
 import firebase from "../../utils/Firebase";
 import "firebase/firestore";
 import "firebase/storage";
@@ -12,8 +13,8 @@ function Album(props) {
   const [album, setAlbum] = useState(null);
   const [imageUrl, setImageUrl] = useState(null);
   const [artist, setArtist] = useState(null);
-  console.log(album);
-  console.log(artist);
+  //console.log(album);
+  //console.log(artist);
   useEffect(() => {
     db.collection("albums")
       .doc(match.params.id)
@@ -43,10 +44,37 @@ function Album(props) {
         });
     }
   }, [album]);
+
+  if (!artist || !album) {
+    return <Loader active>Loading...</Loader>;
+  }
   return (
-    <div>
-      <h2>its for single Album</h2>
+    <div className="album">
+      <div className="album__header">
+        <HeaderAlbum album={album} imageUrl={imageUrl} artist={artist} />
+      </div>
+      <div className="album__songs">
+        <p>here songs</p>
+      </div>
     </div>
   );
 }
 export default withRouter(Album);
+
+function HeaderAlbum(props) {
+  const { album, artist, imageUrl } = props;
+  return (
+    <div>
+      <div
+        className="image"
+        style={{ backgroundImage: `url('${imageUrl}')` }}
+      />
+      <div className="info">
+        <h1>{album.name}</h1>
+        <p>
+          By: <span>{artist.name}</span>
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/pages/Artist/Artist.js
+++ src/pages/Artist/Artist.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { withRouter } from "react-router-dom";
 import firebase from "../../utils/Firebase";
+import { map } from "lodash";
 import "firebase/firestore";
 import "./Artist.scss";
 import BannerArtist from "../../components/Artists/BannerArtist";
@@ -9,15 +10,34 @@ const db = firebase.firestore(firebase);
 function Artist(props) {
   const { match } = props;
   const [artist, setArtist] = useState(null);
-
+  const [albums, setAlbums] = useState([]);
+  console.log(albums);
   useEffect(() => {
     db.collection("artists")
       .doc(match.params.id)
       .get()
       .then(response => {
-        setArtist(response.data());
+        const data = response.data();
+        data.id = response.id;
+        setArtist(data);
       });
   }, [match]);
+  useEffect(() => {
+    if (artist) {
+      db.collection("albums")
+        .where("artist", "==", artist.id)
+        .get()
+        .then(response => {
+          const arrayAlbums = [];
+          map(response.docs, album => {
+            const data = album.data();
+            data.id = album.id;
+            arrayAlbums.push(data);
+          });
+          setAlbums(arrayAlbums);
+        });
+    }
+  }, [artist]);
 
   return (
     <div className="artist">
